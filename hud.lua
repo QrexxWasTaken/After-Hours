22ee57780e1e08c9e2a662f53d51012a395fb8d0
Qrexx <59852162+QrexxWasTaken@users.noreply.github.com>
2022-04-15 14:17:21 +0000

    Added fps in top left corner

--- hud.lua
+++ hud.lua
@@ -2,14 +2,16 @@
 
 hud = {}
 
+local font_fps = love.graphics.newFont("assets/fonts/font_fps.ttf", 18)
+
 function hud:load()
   self.fps = 0
 end
 
 function hud:update(dt)
-  self.fps = dt
+  self.fps = love.timer.getFPS()
 end
 
 function hud:draw()
-  print("fps:" , self.fps)
+  love.graphics.print(self.fps.."  Fps", font_fps, 10, 10)
 end
--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ camera = require("libraries/camera")
 wf = require("libraries/windfield")
 vector = require("libraries/vector")
 require("hud")
-dialove = require("libraries/dialove")
 bigJim = require("characters/bigJim")
 scene = require("libraries/SceneManager")
 
